291d0557701940fac1e030d93dccdf6443458514
ilboued <ilboued@me.com>
2026-05-05 15:06:12 +0200

    Initial Commit

---
+++ test fluidité/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  test fluidité
+//
+//  Created by Jérôme Binachon on 05/05/2026.
+//
+
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
